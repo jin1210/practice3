00fb2cc2c13b4f51ae73ab9e15290d7049e3095b
jin1210 <jinj86530@gmail.com>
2022-09-30 12:23:54 +0000

    [update]

---
+++ app/javascript/app.js
@@ -0,0 +1,34 @@
+$(function(){
+  $('.box1').css({
+    'background-color': '#0000FF',
+    'height' : '100px'
+  });
+});
+
+$('セレクタ').css('backfround-color', '#0000FF');
+
+複数個ある場合
+$('セレクタ').css({
+  'プロパティ1':'値1',
+  'プロパティ2':'値2',
+  :
+  :
+  
+})
+
+$(function(){
+  $('.box1').slideDown();
+});
+
+$(function(){
+  $('.box1').slideUp();
+}
+
+$(function(){
+  $('.box1').show();
+  $('.box1').css({'background-color' : '#0000FF'});
+}
+
+$(function(){
+  $('.box1').hede();
+});
