327e9e797c8d82158e28f9eb9d7735fae78f0cf5
jin1210 <jinj86530@gmail.com>
2022-10-01 12:13:38 +0000

    [update]

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -32,3 +32,48 @@ $(function(){
 $(function(){
   $('.box1').hede();
 });
+
+$(function(){
+  $('.box2').slideDown(function(){
+  $('.box2').css({
+    'background-color' : '#0000FF',
+    'width' : '200px',
+    'height' : '100px',
+  }).slideUp();
+  });
+});
+
+$(function(){
+  $('.box3').mouseover(function(){
+    $('.box3').css({'background-color' : '#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color':'#FF0000'});
+  });
+});
+
+$('.セレクタ名').イベント名(function(){
+  イベント発生時に行われる処理
+});
+
+$(function(){
+  $('.box3').mouseover(function(){
+    $('.box3').addClass('box1-ext');
+  });
+  $('.box3').mouseout(function(){
+    $('.box3').removeClass('box1-ext');
+  });
+});
+
+$('.セレクタ名').on('click',function(){
+  イベント発生時に行われる処理
+});
+
+$(function(){
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+})
