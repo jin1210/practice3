1119e28d816bac4f7503bc9792cff59c7f95376d
jin1210 <jinj86530@gmail.com>
2022-10-04 10:18:50 +0000

    [update]

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,12 +1,8 @@
-$(".theTarget").slippr({
-  transition : 'fade',
-  speed : 1000,
-  easing : 'easeOutQuart',
-  navType : 'block',
-  childrenElementType : 'div',
-  arrows : true,
-  autoPlay : false,
-  autoPlayDuration : 5000,
-  keyboardOnAlways : true,
-  hidePrevious : false
+$(function(){
+  $('#back a').on('click',(event){
+    $('body,html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
 });
